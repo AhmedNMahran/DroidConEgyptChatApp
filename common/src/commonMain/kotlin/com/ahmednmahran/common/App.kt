13c9aa468f9c9634c924f3f24c1abd5fc25f658c
Ahmed Nabil <ahmed.mahran@vodafone.com>
2022-10-15 22:08:21 +0200

    Apply simple navigation logic based on successful login

--- common/src/commonMain/kotlin/com/ahmednmahran/common/App.kt
+++ common/src/commonMain/kotlin/com/ahmednmahran/common/App.kt
@@ -3,29 +3,33 @@ package com.ahmednmahran.common
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.ahmednmahran.common.data.LoginRepository
+import com.ahmednmahran.common.ui.chat.ChatScreen
 import com.ahmednmahran.common.ui.loginscreen.LoginScreen
+import com.github.ahmednmahran.common.model.ChatUser
 
 @Composable
-fun App(modifier: Modifier = Modifier) {
-    var text by remember { mutableStateOf("Hello, DroidConEgypt!") }
-    val platformName = getPlatformName()
+fun App(modifier: Modifier = Modifier, loginRepository: LoginRepository = LoginRepository()) {
+    val chatUser = loginRepository.user.collectAsState()
     Box {
+        if (chatUser.value.username.isBlank()) {
+            LoginScreen(
+                modifier.padding(32.dp).wrapContentSize(Alignment.Center).background(
+                    color = Color(204, 153, 255),
+                    shape = RoundedCornerShape(16.dp)
+                )
+            ) { chatUser ->
+                loginRepository.login(chatUser)
 
-        LoginScreen(
-            modifier.padding(32.dp).wrapContentSize(Alignment.Center).background(
-                color = Color(204, 153, 255),
-                shape = RoundedCornerShape(16.dp)
-            )
-        ) { chatUser ->
+            }
+        }
+        else{
+            ChatScreen(chatUser as MutableState<ChatUser>)
         }
     }
 }

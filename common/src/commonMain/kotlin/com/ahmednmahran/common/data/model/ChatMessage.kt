45f8c3ba7cbd2e406623af22bd5aa0cac8ef342e
Ahmed Nabil <ahmed.mahran@vodafone.com>
2022-10-15 23:52:34 +0200

    update ChatMessage.kt: change sender type from String to ChatUser

--- common/src/commonMain/kotlin/com/ahmednmahran/common/data/model/ChatMessage.kt
+++ common/src/commonMain/kotlin/com/ahmednmahran/common/data/model/ChatMessage.kt
@@ -1,6 +1,7 @@
 package com.ahmednmahran.common.data.model
 
+import com.github.ahmednmahran.common.model.ChatUser
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ChatMessage(val body: String="", val sender : String="",)
+data class ChatMessage(val body: String="", val sender : ChatUser,)

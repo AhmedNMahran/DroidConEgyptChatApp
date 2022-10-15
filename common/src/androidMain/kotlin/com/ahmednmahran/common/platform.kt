1fa5d34323388b8494547e38493f87a6cb32e7c0
Ahmed Nabil <ahmed.mahran@vodafone.com>
2022-10-15 19:56:12 +0200

    add expect actual getHost and ProfileImage functions to platform.kt

--- common/src/androidMain/kotlin/com/ahmednmahran/common/platform.kt
+++ common/src/androidMain/kotlin/com/ahmednmahran/common/platform.kt
@@ -1,5 +1,22 @@
 package com.ahmednmahran.common
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import coil.compose.AsyncImage
+
 actual fun getPlatformName(): String {
     return "Android"
+}
+actual fun getHost(): String {
+    return "10.0.2.2"
+}
+
+@Composable
+actual fun ProfileImage(modifier: Modifier, url: String) {
+    println("android image")
+    AsyncImage(
+        model = url,
+        contentDescription = null,
+        modifier = Modifier
+    )
 }
--- common/src/commonMain/kotlin/com/ahmednmahran/common/platform.kt
+++ common/src/commonMain/kotlin/com/ahmednmahran/common/platform.kt
@@ -1,3 +1,12 @@
 package com.ahmednmahran.common
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 expect fun getPlatformName(): String
+
+
+expect fun getHost(): String
+
+@Composable
+expect fun ProfileImage(modifier: Modifier, url: String)

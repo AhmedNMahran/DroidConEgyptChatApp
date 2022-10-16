ad93dcaf2062bbcd78c0c1b3470e58a7f7b46d18
Ahmed Nabil <ahmed.mahran@vodafone.com>
2022-10-16 02:38:21 +0200

    add Alert.kt to show when the user logs in

---
+++ common/src/commonMain/kotlin/com/ahmednmahran/common/ui/chat/Alert.kt
@@ -0,0 +1,32 @@
+package com.ahmednmahran.common.ui.chat
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+/**
+ * create by @author AhmedNMahran
+ */
+@Composable
+fun Alert(alert: String) {
+    Card(elevation = 8.dp) {
+        Box(
+            modifier = Modifier.background(MaterialTheme.colors.error).clip(
+                RoundedCornerShape(16.dp)
+            )
+        ) {
+            Text(
+                text = alert,
+                color = Color.White,
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
+}
